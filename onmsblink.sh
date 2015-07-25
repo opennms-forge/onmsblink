db879ff749669b3dcc06b1c81c6111a699b3b804
Ronny Trommer <ronny@opennms.org>
2015-07-25 16:34:38 +0200

    Use libraries path in onmsblink without copying to blink1 dir

--- onmsblink.sh
+++ onmsblink.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Djava.library.path=blink1 -cp blink1/blink1.jar:target/onmsblink-1.0-SNAPSHOT-jar-with-dependencies.jar org.opennms.onmsblink.OnmsBlink $@
+java -Djava.library.path=blink1/libraries -cp blink1/libraries/blink1.jar:target/onmsblink-1.0-SNAPSHOT-jar-with-dependencies.jar org.opennms.onmsblink.OnmsBlink $@
