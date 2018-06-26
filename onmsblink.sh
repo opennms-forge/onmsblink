65d7ffc988f8f8cc1201fcd4976467ce9fea1a87
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2018-06-26 07:57:29 +0200

    Changed submodule

--- onmsblink.sh
+++ onmsblink.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd `dirname $0`
-java -Djava.library.path=blink1/libraries -cp blink1/libraries/blink1.jar:target/onmsblink-1.0-SNAPSHOT-jar-with-dependencies.jar org.opennms.onmsblink.OnmsBlink "$@"
+java -Djava.library.path=blink1-java/build -cp blink1-java/build/blink1.jar:target/onmsblink-1.0-SNAPSHOT-jar-with-dependencies.jar org.opennms.onmsblink.OnmsBlink "$@"
